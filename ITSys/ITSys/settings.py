54a5e9ab904c3015d1882f45bbb1aa95358aa6cc
Nicolas Deleu <n.deleu@hotmail.fr>
2023-06-23 16:26:49 +0200

    add throttling

--- ITSys/ITSys/settings.py
+++ ITSys/ITSys/settings.py
@@ -131,6 +131,14 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 AUTHENTICATION_BACKENDS = ['authentication.auth_backends.EmailBackend']
 
 REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    },
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
